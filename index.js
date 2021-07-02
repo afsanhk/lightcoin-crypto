e058d6dc5ff8708e5bc851b3d1bab05c2863b892
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-02 00:36:26 +0000

    Created some classes and inheritance relations as well as states and behaviors to simulate tracking account behaviour for a financial instution (withdrawals and behaviors as subclasses of transaction superclass). Learned about Dependency Interjection!

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions=[];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance () {
+    let balance = 0;
+    this.transactions.forEach(transaction => balance+= transaction.value)
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+  commit(){
+    if(this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      return false
+    }
   }
 
 }
 
+class Deposit extends Transaction{
 
+  get value () {
+    return this.amount;
+  }
 
+  isAllowed () {
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() { // The value of a deposit is negative of the amount. This can be referred to with this.value.
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return this.amount <= this.account.balance
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// NOTES:
+// Passing an object the information it needs when we create it is a great design pattern.
+// This is called Dependency Interjection!
+// In this case, withdrawal and deposits can be associated to any account and not dependent on any surrounding data, only on the object passed in.
+const myAccount = new Account("snow-patrol");
 
-console.log('Balance:', balance);
+console.log('Starting Account Balance: ', myAccount.balance);
+
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
